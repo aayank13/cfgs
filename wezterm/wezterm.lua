beca6f719b974879a454b0185d38d4e15f1e01d7
aayank13 <aayank1306@gmail.com>
2026-04-19 18:04:17 +0530

    wezterm cfg updated

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,31 +6,30 @@ return {
   -- Font
   font = wezterm.font("JetBrainsMono Nerd Font"),
   font_size = 14.0,
+  line_height = 0.95,
 
-  -- Appearance
-  color_scheme = "Catppuccin Mocha",
-  window_background_opacity = 0.92,
+  color_scheme = "Everblush (Gogh)",
+  window_background_opacity = 0.95,
   macos_window_background_blur = 20,
 
-  -- Renderer (OpenGL is more stable on macOS for this issue)
   front_end = "OpenGL",
 
-  -- Remove padding
+  -- use_resize_increments = true,
+
   window_padding = {
-    left = 10,
-    right = 10,
-    top = 10,
-    bottom = 10,
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
   },
 
-  -- Prevent weird resizing behavior
   adjust_window_size_when_changing_font_size = false,
 
-  -- Clean UI
   hide_tab_bar_if_only_one_tab = true,
   use_fancy_tab_bar = false,
   enable_scroll_bar = false,
-  -- window_decorations = "RESIZE",
+  enable_tab_bar = false,
+  scrollback_lines = 35000,
 
   default_cwd = wezterm.home_dir,
 
